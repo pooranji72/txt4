ca91b2d653150c69b55e7884a72c7527d480988f
pooranji72 <pooran.ji.72@gmail.com>
2025-01-25 15:49:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 # -*- coding: utf-8 -*-
-# (c) ACE 
+
+# (c) ACE
 
 import os
 
 class Config(object):
-    # get a token from @BotFather
-    pass
-    """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6305751172:AAG3f0lLXE-ivB1r3xP0VSvUfR0nKfURYfM")
-    API_ID = int(os.environ["API_ID", 29759544]
-    API_HASH = os.environ["API_HASH", "97030a871720998c6efc5debcce67aa4"]
-    AUTH_USERS = "6952370497"""
 
+    # Bot token obtained from @BotFather
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+
+    # API ID and API Hash from Telegram (get them from https://my.telegram.org/auth)
+    API_ID = int(os.environ.get("API_ID", "YOUR_API_ID"))
+    API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+
+    # Optionally, you can add a list of authorized users (user IDs)
+    # This is useful for controlling who can use the bot
+    AUTH_USERS = os.environ.get("AUTH_USERS", "YOUR_USER_IDS").split(",")
+
+    # If you have an access token for the ClassPlus API, include it here
+    CLASSPLUS_ACCESS_TOKEN = os.environ.get("CLASSPLUS_ACCESS_TOKEN", "YOUR_CLASSPLUS_ACCESS_TOKEN")
+    
